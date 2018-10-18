2d937a1a457a0f787d3116f5d8ce481d1239ffc3
Chris Oldwood <gort@cix.co.uk>
2018-10-18 20:56:08 +0100

    Bumped copyright and version for initial release.

--- Source/Mechanisms/Properties/AssemblyInfo.cs
+++ Source/Mechanisms/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Mechanisms")]
-[assembly: AssemblyCopyright("Copyright © Chris Oldwood 2017")]
+[assembly: AssemblyCopyright("Copyright © Chris Oldwood 2017-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
