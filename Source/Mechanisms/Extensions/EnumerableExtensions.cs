f70114fbd1f5303cb95eb1e64bdc18d58836e24a
Chris Oldwood <gort@cix.co.uk>
2026-04-07 19:15:25 +0100

    Added parallel solution and project files for .Net Core

--- Source/Mechanisms/Extensions/EnumerableExtensions.cs
+++ Source/Mechanisms/Extensions/EnumerableExtensions.cs
@@ -4,9 +4,11 @@ namespace Mechanisms.Extensions
 {
     public static class EnumerableExtensions
     {
+#if !NETSTANDARD2_0_OR_GREATER
         public static HashSet<T> ToHashSet<T>(this IEnumerable<T> sequence)
         {
             return new HashSet<T>(sequence);
         }
+#endif
     }
 }
